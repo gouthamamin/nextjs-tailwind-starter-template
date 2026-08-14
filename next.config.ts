bb74aa18f904400133070f8cb79b301ab1ff5a9b
Goutham_Amin <gouthamamin@gmail.com>
2026-08-14 13:15:39 +0530

    chore: update next config for ouput folder

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: process.env.NEXT_ALLOWED_DEV_ORIGIN ? [process.env.NEXT_ALLOWED_DEV_ORIGIN] : [],
+  output: "export"
 };
 
 export default nextConfig;
